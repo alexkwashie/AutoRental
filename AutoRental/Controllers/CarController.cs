acdc47ff0e2a0f9cdfdca9b75cf2efcef2fb1f45
alexkwashie <alex.kwashie@hotmail.com>
2019-06-21 21:53:46 +0100

    MVC in Action

---
+++ AutoRental/Controllers/CarController.cs
@@ -0,0 +1,20 @@
+﻿using AutoRental.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AutoRental.Controllers
+{
+    public class CarController : Controller
+    {
+        // GET: Car
+        public ActionResult Random()
+        {
+            var car = new Car() { Name = "Audi" };
+
+            return View(car); //car here to render it in on the wepage
+        }
+    }
+}
